a37c546416858eecd9ed05337fa8b85c80a522b7
pabloFuente <pablofuenteperez@gmail.com>
2016-04-25 09:34:01 +0200

    HTML Generator begun

--- ProyectoJava/PascalAnalyzer/src/HTMLGenerator.java
+++ ProyectoJava/PascalAnalyzer/src/HTMLGenerator.java
@@ -1,4 +1,11 @@
 import com.sun.org.apache.xpath.internal.SourceTree;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 
 public class HTMLGenerator {
 
@@ -10,7 +17,7 @@ public class HTMLGenerator {
                                         ".ident {color:rgb(55,40,244);}\n" +
                                         ".palres {color:rgb(0,0,0);font-weight:bold;}\n" +
                                         "</style>\n" +
-                                        "<style type=\"text/css\"></style></head>;}\n" +
+                                        "<style type=\"text/css\"></style></head>\n" +
                                         "<body>\n";
 
     static String cierreHTML =  "\n</body>\n" +
@@ -29,6 +36,35 @@ public class HTMLGenerator {
     public String closeHTML (String... prgSentence){
         pushHTML(prgSentence);
         System.out.println(cabeceraHastaBody + innerHTML + cierreHTML);
+        createHtml(cabeceraHastaBody + innerHTML + cierreHTML);
         return cabeceraHastaBody + innerHTML + cierreHTML;
+    }
+
+    public String getIdent (String s){
+        return "<span class='ident'>" + s + "</span>";
+    }
+
+    /**
+     * Este método creara un archivo html listo para ser visualizado
+     */
+    public void createHtml(String html){
+        Writer out = null;
+        try {
+            out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("programa.html"), "UTF-8"));
+            try {
+                out.write(html);
+            } catch (IOException ex) {
+                System.out.println("Mensaje excepcion escritura: " + ex.getMessage());
+            }
+
+        } catch (UnsupportedEncodingException | FileNotFoundException ex2) {
+            System.out.println("Mensaje error 2: " + ex2.getMessage());
+        } finally {
+            try {
+                out.close();
+            } catch (IOException ex3) {
+                System.out.println("Mensaje error cierre fichero: " + ex3.getMessage());
+            }
+        }
     }
 }
